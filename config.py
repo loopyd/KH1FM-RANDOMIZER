1705b524fb63615bd3df2ad13f90002590d5da3e
loopyd <nightwintertooth@gmail.com>
2026-02-19 23:21:09 -0800

    Minor fixup

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ NodeType = Literal["mod", "settings", "seed"]
 
 def read_presets(node_name: NodeType) -> Dict:
     presets_path = root_path().joinpath(f"{node_name}_generator_presets.json")
-    data = read_json(presets_path)
+    data = read_json(file_path=presets_path, ask_prompt=False)
     return data
 
 def write_presets(node_name: NodeType, args: Namespace) -> None:
@@ -18,4 +18,4 @@ def write_presets(node_name: NodeType, args: Namespace) -> None:
         pythonic_vars = {
             key: value for key, value in pythonic_vars.items() if key in ["ap_zip_file", "kh1_data_path"]
         }
-    write_json(presets_path, pythonic_vars, overwrite=True)
+    write_json(presets_path, pythonic_vars, overwrite=True, create_parents=True)
